656201376fae9960aa7e790ebaf41c1af7155410
yugeshkanna1919 <yugeshkannajagathesan@gmail.com>
2025-06-19 16:10:29 +0530

    Day3

---
+++ learn/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WElcome from './Welcome.jsx'
+import Welcome from './Welcome.jsx'
+
+function App() {
+  return (
+
+    <>
+    <Welcome name="Yugesh"  page="home"/>
+    <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ script.js
@@ -0,0 +1,8 @@
+function add(a,b){
+    return a+b
+}
+const multiply = (a,b) => {
+    return a*b
+}
+console.log(add(2,3)); // 5
+console.log(multiply(2,3)); // 6
